f911ce595d5d1c8f2f3e42e96c3d6574b0e86c4f
Ryan Murthick <ryan.murthick@clock.co.uk>
2019-07-02 14:47:49 +0100

    feat(schemata): Alternative method to transpiling babel

--- test/property-name.test.js
+++ test/property-name.test.js
@@ -17,11 +17,8 @@ describe('#propertyName()', () => {
     const schema = createContactSchema()
     const propertyName = 'Wobble'
 
-    assert.throws(
-      () => {
-        schema.propertyName(propertyName)
-      },
-      { message: `No property '${propertyName}' in schema` }
-    )
+    assert.throws(() => {
+      schema.propertyName(propertyName)
+    }, `No property '${propertyName}' in schema`)
   })
 })
